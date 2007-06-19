874fff3c209a22415d9e4722b3cd54a0ffa07e7c
Jonathan Rockway <jon@jrock.us>
2007-06-19 02:39:27 -0500

    add test for two forms

--- t/canary-insertion.t
+++ t/canary-insertion.t
@@ -3,7 +3,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 6;
+use Test::More tests => 9;
 use Test::WWW::Mechanize::Catalyst 't::lib::TestApp';
 
 my $basic_form = t::lib::TestApp::Controller::Root::basic_form();
@@ -16,3 +16,8 @@ is($mech->content, $basic_form, 'not_html is not modified');
 $mech->get_ok('http://whatever/one_form');
 isnt($mech->content, $basic_form, 'one_form is modified');
 $mech->content_like(qr/name="canary_[^"]+"/, 'contains canary');
+
+my $two_basic_forms = t::lib::TestApp::Controller::Root::two_basic_forms();
+$mech->get_ok('http://whatever/two_forms');
+isnt($mech->content, $two_basic_forms, 'two_forms is modified');
+$mech->content_like(qr{(name="canary_[^"]+").*\1}s,'contains 2 canaries');
--- t/lib/TestApp/Controller/Root.pm
+++ t/lib/TestApp/Controller/Root.pm
@@ -19,6 +19,12 @@ sub one_form :Local {
     $c->res->body(basic_form());
 }
 
+sub two_forms :Local {
+    my ($self, $c) = @_;
+    $c->res->content_type('text/html');
+    $c->res->body(two_basic_forms());
+}
+
 sub basic_form {
     return<<'HTML'
 <html>
@@ -27,6 +33,24 @@ sub basic_form {
  <h1>Hello</h1>
  <p>This is a test page.  If you are seeing this you are weird.</p>
  <form action="who cares" method="also not relevant">
+  <input type="hidden" name="foo" value="foo" id="foo" />
+ </form>
+</body>
+</html>
+HTML
+}
+
+sub two_basic_forms {
+        return<<'HTML'
+<html>
+<head><title>Test</title></head>
+<body>
+ <h1>Hello</h1>
+ <p>This is a test page.  If you are seeing this you are weird.</p>
+ <form action="who cares" method="also not relevant">
+  <input type="hidden" name="foo" value="foo" id="foo" />
+ </form>
+ <form action="another" method="POST">
   <input type="hidden" name="foo" value="foo" id="foo" />
  </form>
 </body>
